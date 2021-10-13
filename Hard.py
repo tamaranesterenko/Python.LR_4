b54188a40ca461f77813e5162ae8f04bb185fef0
tamaranesterenko <90053368+tamaranesterenko@users.noreply.github.com>
2021-10-13 22:05:21 +0300

    Add hard task

---
+++ Hard.py
@@ -0,0 +1,7 @@
+radians = float(input("Enter the radians: "))
+hour = radians // 30
+minutes = radians % 30
+print("Degrees = ", radians)
+print("Hour ",  round(hour, 0))
+print("Minutes ", minutes)
+
